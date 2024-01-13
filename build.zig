7f5cc0cc3d36545a6f9bcf34c1b5cc4f06a3cd56
Ben Crist <github@magicmoremagic.com>
2024-01-12 21:05:32 -0600

    update for zig build system changes

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("deep_hash_map", .{
-        .source_file = .{ .path = "deep_hash_map.zig" },
+        .root_source_file = .{ .path = "deep_hash_map.zig" },
     });
 }
